030c39129ebbad58abae0c0866dd1e820b4f56be
totargaming <totargaming@gmail.com>
2024-02-26 12:54:04 +1030

    modified

--- count.cpp
+++ count.cpp
@@ -1,8 +1,10 @@
+#include <iostream>
+
 int count(int array[], int n) {
-  int sum{};
+  int sum = 0;
   for (int i = 0; i < n; i++) {
     if (array[i] % 2 == 0) {
-      sum += array[i];
+      sum++;
     }
   }
   return sum;
