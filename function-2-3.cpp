0e401e25899ddd5a2b92deffe36b09187bb5ee9d
totargaming <totargaming@gmail.com>
2024-02-28 16:49:34 +1030

    finish 3-4 4-5

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void count_numbers(int array[], int n) {
+void two_five_nine(int array[], int n) {
   int num_twos = 0, num_fives = 0, num_nines = 0;
 
   for (int i = 0; i < n; i++) {
