f98507af99cedfdc15ad305e0be05e9d0966ad71
totargaming <totargaming@gmail.com>
2024-03-05 02:11:23 +1030

    fix f2-2

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -3,7 +3,7 @@
 int binary_to_int(int binary_digits[], int number_of_digits) {
   int res{};
 
-  for (int i = number_of_digits - 1; i >= 0; i++) {
+  for (int i = 0; i < number_of_digits; i++) {
     res += binary_digits[i] * std::pow(2, number_of_digits - i - 1);
   }
   return res;
